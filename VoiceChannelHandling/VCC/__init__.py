84bb34ad4d4d7b3c4b69141e2db5f7aa01b18968
lima12 <igihai@gmail.com>
2025-11-22 19:31:56 -0500

    add VC Owner command declare

--- VoiceChannelHandling/VCC/__init__.py
+++ VoiceChannelHandling/VCC/__init__.py
@@ -1,3 +1,4 @@
 from .commands_mixin import VCCCommandsMixin
+from .VCOwnerCommand import VCOwnerCommandsMixin
 
-__all__ = ["VCCCommandsMixin"]
+__all__ = ["VCCCommandsMixin", "VCOwnerCommandsMixin"]
